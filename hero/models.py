ec7f18b0bc99780b84273b3ad95f79a1ee41d1a6
Smetankin <elcapitansmetankins@yandex.ru>
2024-11-01 00:55:53 +0100

    work-in-progress::hero-stats

--- hero/models.py
+++ hero/models.py
@@ -10,5 +10,16 @@ class Hero(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, index=True)
+
     level = Column(Integer, default=1)
-    player_id = Column(Integer, ForeignKey('players.id'))
+    experience = Column(Integer, default=0)
+    health = Column(Integer)
+
+    strength = Column(Integer)
+    perception = Column(Integer)
+    agility = Column(Integer)
+    research = Column(Integer)
+    charisma = Column(Integer)
+    lucky = Column(Integer)
+
+    user_id = Column(Integer, ForeignKey('players.id'))
