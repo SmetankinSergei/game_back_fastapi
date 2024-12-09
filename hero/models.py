4a7a3335bd84fc47d3a0761e6aa63f95f90c0f29
Smetankin <elcapitansmetankins@yandex.ru>
2024-12-09 02:21:32 +0100

    work-in-progress::inventory-model

--- hero/models.py
+++ hero/models.py
@@ -65,7 +65,7 @@ class InventoryItem(Base):
     # настройка веса
     # настройка места
     # настройка уровня
-    # сравнение инвенторей
+    # сравнение инвентарей
     # base manager class
 
     hero = relationship('Hero', back_populates='inventory_items')
